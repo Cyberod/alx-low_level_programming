c5a8f60b5ca080486520a33883db538a7d38266d
Cyberod <jonatkeez@gmail.com>
2022-09-08 09:09:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Author: Cyberod
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[70] = "\"Programming is like building a mutilingual puzzle\"";
+
+	puts(string);
+
+	puts("\"Programming is like building a mutilingual puzzle\"");
+
+	return (0);
+}
