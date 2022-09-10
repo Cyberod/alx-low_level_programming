e83e974ff7530592c99bc22b9d527ac8f63187d5
Cyberod <jonatkeez@gmail.com>
2022-09-10 12:48:21 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; j++)
+	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
@@ -18,7 +18,7 @@ int main(void)
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j /10) + 48);
+				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
