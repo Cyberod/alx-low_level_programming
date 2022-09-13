73aa20967f32708bd49245f474d18d1c86ba9358
Cyberod <jonatkeez@gmail.com>
2022-09-13 14:32:41 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,7 +26,7 @@ void jack_bauer(void)
 			{
 				_putchar(':');
 				_putchar('0');
-				_putchar(j + '0');
+				_putchar(j + '0Ri;
 			}
 			else if (j >= 10)
 			{
@@ -34,7 +34,7 @@ void jack_bauer(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
