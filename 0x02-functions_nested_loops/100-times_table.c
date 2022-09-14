75d79678e11879238745d29d5d54a9b07ea45c56
Cyberod <jonatkeez@gmail.com>
2022-09-13 22:43:02 -0700

    good

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,13 +30,13 @@ void print_times_table(int n)
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
-			else if (rep > 100)
+			else
 			{
 				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 100);
 				_putchar('0' + rep % 10);
 			}
-			else if (b < n)
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
