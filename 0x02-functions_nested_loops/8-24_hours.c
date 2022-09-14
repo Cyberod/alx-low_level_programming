aead563dbec319e62a5a0004d86717998516d524
Cyberod <110993874+Cyberod@users.noreply.github.com>
2022-09-14 05:53:58 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,39 +2,28 @@
 
 /**
  * jack_bauer -> prints 24 hours
- * Returns: Always 0 (Sucess)
+ *
+ * Return: Always 0 (Success)
  */
+
 void jack_bauer(void)
+
 {
 	int i, j;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			if (i < 10)
-			{
-				_putchar('0');
-				_putchar(i + '0');
-			}
-			else if (1 >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if (j < 10)
-			{
-				_putchar(':');
-				_putchar('0');
-				_putchar(j + '0Ri;
-			}
-			else if (j >= 10)
-			{
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+			_putchar (i / 10 + '0');
+			_putchar (i % 10 + '0');
+			_putchar (':');
+			_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+			_putchar ('\n');
+
 		}
-		_putchar('\n');
+
 	}
+
 }
