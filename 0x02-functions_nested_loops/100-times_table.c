2149c252a91da2774ba75f18b707dffb778b2d7a
Cyberod <jonatkeez@gmail.com>
2022-09-14 20:58:08 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,9 @@ void print_times_table(int n)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			for (column = 0; column <= n; column ++)
+			for (column = 0; column <= n; column++)
 			{
 				product = row * column;
-				
 				if (column == 0)
 					_putchar('0');
 				else if (product < 10)
@@ -33,7 +32,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + 0);
+					_putchar(product % 10 + '0');
 				}
 				else if (product > 99 && product < 1000)
 				{
