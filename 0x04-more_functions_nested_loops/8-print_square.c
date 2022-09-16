fa6055046895e100f0d327619591d247c49a2d5f
Cyberod <jonatkeez@gmail.com>
2022-09-16 23:35:04 +0100

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,9 +17,9 @@ void print_square(int size)
 		{
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
-				putchar('0');
+				putchar('#');
 			}
-			putchar('0');
+			putchar('#');
 			putchar('\n');
 		}
 	}
