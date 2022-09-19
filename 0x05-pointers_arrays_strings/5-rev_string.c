bc788ef3586736d92690b01f17c6d5ef109972fb
Cyberod <110993874+Cyberod@users.noreply.github.com>
2022-09-19 19:09:28 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+/**
+ * rev_string -> Writes a function that reverses a string
+ * @s: Takes a string parameter
+ * Return: Success 1
+ */
+void rev_string(char *s)
+{
+char tmp;
+int i, length1, length2;
+
+length1 = 0;
+length2 = 0;
+
+while (s[length1] != '\0')
+length1++;
+
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2; i++)
+{
+tmp = s[i];
+s[i] = s[length2];
+s[length2] = tmp;
+length2 -= 1;
+}
+}
