c7ff46c4f437f2050321215b2ccc4c43c39d2521
Cyberod <110993874+Cyberod@users.noreply.github.com>
2022-09-21 22:52:35 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,18 +4,18 @@
  * print_rev -> prints a string in reverse
  * @s: the string to be printed in reverse
  */
-void print_rev(char *s)
+void print_rev(char *s);
 {
-int i, n;
+        int i, n;
 
-n = 0;
+        n = 0;
 
-while (s[n] != '\0')
-n++;
+        while (s[n] != '\0')
+                n++;
 
-for (i = n - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+        for (i = n - 1; i >= 0; i--)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
 }
