31a25ee0795a14e12f282f2eec90e9ccee482665
Cyberod <jonatkeez@gmail.com>
2022-10-10 21:20:00 +0100

    Defined a new type: struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -> user defined data type
+ * @name: name of the dog
+ * @age: Age of the dog
+ * @owner: The owner of the dog
+ * Author: Jonaz
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
