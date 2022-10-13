eee76bd464c0ef84beee5c10f1f97262f10983ae
Cyberod <110993874+Cyberod@users.noreply.github.com>
2022-10-13 13:35:50 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,6 @@
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
-voiid print_numbers(const char *separators, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
-#endif /* _VARIADIC_FUNCTIONS_ */
+#endif
